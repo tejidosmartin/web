06c88a2ee31622a05bb716e3ce617afcdb7059a6
shakarr <risaaflo@gmail.com>
2022-05-20 00:20:24 +0200

    refactor

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: "https://phpbackendtejidosmartin.herokuapp.com/controller",
+  baseUrl: "https://tejidosmartin.000webhostapp.com/controller",
   mernUrl: "https://tejidosmartin.herokuapp.com/api"
 };
