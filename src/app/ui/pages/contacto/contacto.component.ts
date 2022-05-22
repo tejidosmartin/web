b980f4f6ccfab834b240669df3439b06d956e0d7
shakarr <risaaflo@gmail.com>
2022-05-22 19:48:23 +0200

    refactorización

--- src/app/ui/pages/contacto/contacto.component.ts
+++ src/app/ui/pages/contacto/contacto.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-contacto',
   templateUrl: './contacto.component.html',
-  styleUrls: ['./contacto.component.css']
+  styleUrls: ['./contacto.component.css'],
 })
 export class ContactoComponent implements OnInit {
+  formContact: FormGroup = this._fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    subject: ['', [Validators.required]],
+    name: ['', [Validators.required]],
+    mesagge: ['', [Validators.required]],
+  });
 
-  titulo:string ="Contactanos"
+  titulo: string = 'Contactanos';
 
-  constructor() { }
+  constructor(private _router: Router, private _fb: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  submit() {
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Gracias por contactarnos',
+      showConfirmButton: false,
+      timer: 1500,
+    });
+    this._router.navigate(['/']);
   }
-
 }
